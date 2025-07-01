181d5c54073f14b7e021d5254d262fbcc7708c7a
AleksejTrimajlov <122823958+AleksejTrimajlov@users.noreply.github.com>
2025-07-01 11:49:24 +0500

    Add files via upload

---
+++ src/pages/_app.js
@@ -0,0 +1,6 @@
+import "@/styles/bases/normalize.css";
+import "@/styles/globals.css";
+import "@/styles/bases/day.scss";
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
