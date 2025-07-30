f6cc9b4bec9fc43ee679cf66cb604c7fb5d68020
AleksejTrimajlov <122823958+AleksejTrimajlov@users.noreply.github.com>
2025-07-30 10:38:14 +0500

    Delete src directory

--- src/pages/_app.js
+++
@@ -1,6 +0,0 @@
-import "@/styles/bases/normalize.css";
-import "@/styles/globals.css";
-import "@/styles/bases/day.scss";
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
